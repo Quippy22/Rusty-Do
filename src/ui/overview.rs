cb503dad4e567df7747cac0bb8791380a8b12b57
Quippy22 <darius.alexandru2202@gmail.com>
2026-02-21 22:47:54 +0200

    ui: implement quick acces for the first notebooks

--- src/ui/overview.rs
+++ src/ui/overview.rs
@@ -1,9 +1,10 @@
+use crossterm::event::{KeyCode, KeyEvent};
 use ratatui::{
-    layout::{ Alignment, Constraint, Direction, Layout, Rect },
-    widgets::{ Block, BorderType, Borders, List, ListState, ListItem, Paragraph },
     Frame,
+    layout::{Alignment, Constraint, Direction, Layout, Rect},
+    style,
+    widgets::{Block, BorderType, Borders, List, ListItem, ListState, Paragraph},
 };
-use crossterm::event::{ KeyCode, KeyEvent };
 
 use crate::models::notebook::Notebook;
 
@@ -15,7 +16,7 @@ pub struct Overview {
 
 pub enum OverviewAction {
     DeleteNotebook,
-    RenameNotebook, 
+    RenameNotebook,
     AccessNotebook,
 }
 
@@ -35,7 +36,7 @@ impl Overview {
         let chunks = Layout::default()
             .direction(Direction::Horizontal)
             .constraints([Constraint::Percentage(50), Constraint::Percentage(50)])
-            .spacing(2)
+            .spacing(1)
             .split(area);
 
         // 2. Left block
@@ -46,12 +47,20 @@ impl Overview {
             .borders(Borders::ALL)
             .border_type(BorderType::Rounded);
 
-        let notebooks: Vec<ListItem> = self.notebooks
+        let notebooks: Vec<ListItem> = self
+            .notebooks
             .iter()
-            .map(|n| ListItem::new(n.name.as_str()))
+            .enumerate()
+            .map(|(index, name)| {
+                let line = format!("{} {}", index + 1, name.name);
+                ListItem::new(line)
+            })
             .collect();
 
-        let notebook_list = List::new(notebooks).highlight_symbol("> ").block(notebooks_block);
+        let notebook_list = List::new(notebooks)
+            .highlight_symbol("> ")
+            .highlight_style(style::palette::tailwind::ROSE.c500)
+            .block(notebooks_block);
         f.render_stateful_widget(notebook_list, chunks[0], &mut self.state);
 
         // 3. Right block
@@ -68,7 +77,8 @@ impl Overview {
             if notebook.tasks.is_empty() {
                 String::from("No tasks yet.")
             } else {
-                notebook.tasks
+                notebook
+                    .tasks
                     .iter()
                     .map(|t| format!("• {}", t.name))
                     .collect::<Vec<String>>()
@@ -102,9 +112,16 @@ impl Overview {
                 self.state.select_last();
                 None
             }
-            KeyCode::Char('d')  => Some(OverviewAction::DeleteNotebook),
-            KeyCode::Enter => Some(OverviewAction::AccessNotebook),
+            KeyCode::Char('d') => Some(OverviewAction::DeleteNotebook),
             KeyCode::Char('r') => Some(OverviewAction::RenameNotebook),
+            KeyCode::Enter => Some(OverviewAction::AccessNotebook),
+            KeyCode::Char(c @ '1'..='5') => {
+                let index: usize = (c.to_digit(10).unwrap_or(0) - 1) as usize;
+                if self.notebooks.len() > index {
+                    self.state.select(Some(index));
+                }
+                Some(OverviewAction::AccessNotebook)
+            }
 
             _ => None,
         }
@@ -117,7 +134,11 @@ impl Overview {
         }
         let i = match self.state.selected() {
             Some(i) => {
-                if i >= self.notebooks.len() - 1 { 0 } else { i + 1 }
+                if i >= self.notebooks.len() - 1 {
+                    0
+                } else {
+                    i + 1
+                }
             }
             None => 0,
         };
@@ -130,7 +151,11 @@ impl Overview {
         }
         let i = match self.state.selected() {
             Some(i) => {
-                if i == 0 { self.notebooks.len() - 1 } else { i - 1 }
+                if i == 0 {
+                    self.notebooks.len() - 1
+                } else {
+                    i - 1
+                }
             }
             None => 0,
         };
